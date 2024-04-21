84887d470ebc48868493a61dd3e9d1f0ffd9d7ff
juan perez <juanperezzdp@gmail.com>
2024-04-21 18:58:39 -0500

    delete import

--- src/app/homeAdmin/page.tsx
+++ src/app/homeAdmin/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import useToken from "@/hooks/useToken";
 import Image from "next/image";
