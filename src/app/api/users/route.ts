7840695adcde097c549cfc88ae80f49f36fec95d
juan perez <juanperezzdp@gmail.com>
2024-04-21 20:52:35 -0500

    config endpoint GET users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -7,11 +7,17 @@ export async function GET(request: NextRequest) {
   try {
     await connectMongoDB();
 
-    await User.updateMany({}, { $set: { updatedAt: new Date() } });
+    // Obtener la fecha de la última solicitud desde el header 'Last-Request'
+    const lastRequestDate = new Date(request.headers.get("Last-Request") || 0);
 
-    const users = await User.find();
+    // Buscar los usuarios creados después de la última solicitud
+    const newUsers = await User.find({ createdAt: { $gt: lastRequestDate } });
 
-    return NextResponse.json({ users }, { status: 200 });
+    // Obtener todos los usuarios
+    const allUsers = await User.find();
+
+    // Devolver los usuarios y la información sobre los nuevos usuarios
+    return NextResponse.json({ users: allUsers, newUsers }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: messages.error.default, error },
