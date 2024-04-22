df9b04c9dbd8152e79dcc676498cd9e2c0df7c1a
juan perez <juanperezzdp@gmail.com>
2024-04-21 21:03:37 -0500

    delete comment

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -7,16 +7,12 @@ export async function GET(request: NextRequest) {
   try {
     await connectMongoDB();
 
-    // Obtener la fecha de la última solicitud desde el header 'Last-Request'
     const lastRequestDate = new Date(request.headers.get("Last-Request") || 0);
 
-    // Buscar los usuarios creados después de la última solicitud
     const newUsers = await User.find({ createdAt: { $gt: lastRequestDate } });
 
-    // Obtener todos los usuarios
     const allUsers = await User.find();
 
-    // Devolver los usuarios y la información sobre los nuevos usuarios
     return NextResponse.json({ users: allUsers, newUsers }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
