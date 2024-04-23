93371671dddb86bb926f5c6cd48027180bc3be69
juan perez <juanperezzdp@gmail.com>
2024-04-23 18:14:58 -0500

    configuration for protection of endpoint user routes and permission

--- src/middleware.ts
+++ src/middleware.ts
@@ -54,5 +54,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/homeUser/:path*", "/homeAdmin/:path*"],
+  matcher: ["/homeUser/:path*", "/homeAdmin/:path*", "/api/:path*"],
 };
