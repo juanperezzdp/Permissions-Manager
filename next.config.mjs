1ae6bf21016c7c87cca06562efe625c2743742bd
juan perez <juanperezzdp@gmail.com>
2024-04-27 09:18:12 -0500

    test usesearchparams

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  experimental: {},
 };
 
 export default nextConfig;
